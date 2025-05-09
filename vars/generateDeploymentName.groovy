3f1efdc6fcf2946412a061a676b17cd4198547e4
Mohammed Bazed Ali <bazed.ali@prevalent.ai>
2025-05-09 13:05:53 +0530

    fixed typo

--- vars/generateDeploymentName.groovy
+++ vars/generateDeploymentName.groovy
@@ -1,4 +1,4 @@
 def call(String appName, String branchName, String buildNumber, String deployDir = '/opt/app_deployments'){
     def safeBranch = branchName.replaceAll('/','-')
-    return \"${deployDir}/${appName}-${safeBranch}-${buildNumber}.jar"
+    return "${deployDir}/${appName}-${safeBranch}-${buildNumber}.jar"
 }
